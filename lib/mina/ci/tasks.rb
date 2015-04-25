09761230bbe2e9aee86796269e07caee54cdeb64
@seandong <sindon@gmail.com>
2015-04-25 13:50:46 +0800

    add CircleCI status checking for mina deploy.

--- lib/mina/ci/tasks.rb
+++ lib/mina/ci/tasks.rb
@@ -1,3 +1,50 @@
+require 'json'
+
+# # Modules: CI
+# Adds settings and tasks for checking CircleCI status.
+#
+#     require 'mina/ci'
+#
+# ## Settings
+#
+# ### circle_token
+# CircleCI API access token.
+#
+# ### circle_username
+# CircleCI username
+#
+# ### circle_project
+# Name by which CircleCI knows your project
+#
+
+set_default :circle_token,     fail('CircleCI token required')
+set_default :circle_username,  fail('CircleCI username required')
+set_default :circle_project,   fail('CircleCI project required')
+
 namespace :ci do
 
+  desc 'Verify CircleCI building status.'
+  task :verify_status => :evironment do
+    queue %[echo "-----> check CircleCI status"]
+    unless %w(success fixed).include?(cricle_status)
+      die 1, "CircleCI not passed (#{cricle_status}), please check and fix problem first."
+    end
+  end
+
+  private
+
+  def circle_url
+    "https://circleci.com/api/v1/project/#{circle_username}/#{circle_project}?circle-token=#{circle_token}"
+  end
+
+  def circle_repsonse
+    JSON.parse `curl -H 'Accept: application/json' #{circle_url}`
+  end
+
+  def cricle_status
+    @cricle_status ||= begin
+      response = circle_repsonse.find { |item| item['branch'] == branch }
+      response.nil? ? '' : response['status']
+    end
+  end
 end
--- mina-ci.gemspec
+++ mina-ci.gemspec
@@ -19,6 +19,7 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency 'bundler', '~> 1.6'
   spec.add_development_dependency 'rake'
+  spec.add_development_dependency "minitest", "~> 5.0"
 
   spec.add_runtime_dependency 'mina', '~> 0.3', '>= 0.3.0'
 end
